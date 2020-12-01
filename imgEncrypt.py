5b8643396295a310c33ae72993094a437c37c726
Marcel Dan <gigi.dan2011@gmail.com>
2020-12-01 09:22:52 +0100

    AÃadido nuevo algoritmo, CTR y corregidos errores

--- imgEncrypt.py
+++ imgEncrypt.py
@@ -2,13 +2,17 @@ import os
 import random
 import hashlib
 import datetime
+import sys
+import random
 
 from Cryptodome.Cipher import AES
 from Cryptodome.Hash import SHA256
 from Cryptodome.Util.Padding import pad
+from Cryptodome.Util import Counter
 
 
-def encrypt(key, filename):
+def encrypt(key, filename, mode):
+    counterRand = random.randint(10, 16)
     chunk_size = 64*1024
     output_file = "ENC"+filename
     file_size = str(os.path.getsize(filename)).zfill(16)
@@ -16,11 +20,14 @@ def encrypt(key, filename):
     for i in range(16):  # PKCS7Padding ya que es de 16 bytes modificables, y no 8 bytes fijos como en PKCS5Padding
         IV += chr(random.randint(0, 0xF))
     begin_time = datetime.datetime.now()
-    encryptor = AES.new(key, AES.MODE_CBC, IV.encode("utf8"))
+    counter = Counter.new(128, initial_value=counterRand)
+    #encryptor = AES.new(key, AES.MODE_GCM, IV.encode("utf8"))
+    encryptor = AES.new(key, AES.MODE_CTR, counter=counter)
     with open(filename, 'rb') as inputfile:
         with open(output_file, 'wb') as outf:
             outf.write(file_size.encode("utf8"))
-            outf.write(IV.encode("utf8"))
+            # outf.write(IV.encode("utf8"))
+            outf.write(str(counterRand).encode("utf8"))
             while True:
                 chunk = inputfile.read(chunk_size)
                 if len(chunk) == 0:
@@ -38,8 +45,11 @@ def decrypt(key, filename):
     begin_time = datetime.datetime.now()
     with open(filename, 'rb') as inf:
         filesize = int(inf.read(16))
-        IV = inf.read(16)
-        decryptor = AES.new(key, AES.MODE_CBC, IV)
+        #IV = inf.read(16)
+        IV = inf.read(2)
+        counter = Counter.new(128, initial_value=int(IV.decode("utf8")))
+        #decryptor = AES.new(key, AES.MODE_GCM, IV)
+        decryptor = AES.new(key, AES.MODE_CTR, counter=counter)
         with open(output_file, 'wb') as outf:
             while True:
                 chunk = inf.read(chunk_size)
@@ -59,17 +69,19 @@ def getKey(password):
 def main():
     choice = int()
     choice = input(
-        "Select One of the following\n> 1. Encrypt \n> 2. Decrypt\n>>> ")
+        "Select One of the following\n> 1. Encrypt \n> 2. Decrypt\n>> ")
     if choice == "1":
         filename = input("Enter the name of file to be encrypted >> ")
-        password = input("Enter the password")
-        encrypt(getKey(password), filename)
+        password = input("Enter the password >> ")
+        mode = input(
+            "Choose what mode you want for encryption:\n1. AES-CBC\n2. AES-GCM\n3. AES-CTR")
+        encrypt(getKey(password), filename, mode)
         print("Done!\n{} ==> {}".format(filename, "ENC" + filename))
     elif choice == "2":
         filename = input("File to be decrypted > ")
         password = input("Password: ")
         decrypt(getKey(password), filename)
-        print("Done!\n{} ==> {}".format(filename, filename[3:]))
+        print("Done!\n{} ==> {}".format(filename, "DEC" + filename[3:]))
     else:
         print("No option Selected")
 
